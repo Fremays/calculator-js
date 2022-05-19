82c4e7d4eb30f36b06f87795404aa3b5596ca7ad
Alexander Shchukin <Shchale@yandex.ru>
2022-05-19 16:34:11 +0300

    Added button to visual resolt, added comments. Programm done!

--- calculation.js
+++ calculation.js
@@ -2,8 +2,8 @@ let operation
 
 function calculation() {
     let result
-    const number1 = Number((document.getElementById("number1").value))
-    const number2 = Number((document.getElementById("number2").value))
+    const number1 = Number((document.getElementById("number1").value)) // Получение первого числа
+    const number2 = Number((document.getElementById("number2").value)) // Получение второго числа
     switch (operation) {
         case "+":
             result = number1 + number2;
@@ -17,5 +17,10 @@ function calculation() {
         case "*":
             result = number1 * number2;
             break;
+    }
+    if (result != undefined) {
+        document.getElementById("result").innerHTML = result;
+    } else {
+        document.getElementById("result").innerHTML = "Не введены все данные!"
     }
 }
